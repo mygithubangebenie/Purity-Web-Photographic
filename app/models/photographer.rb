0e4bc443259803675e984392d38ba6cfe91935fd
mygithubangebenie <angebenie23@gmail.com>
2019-12-05 11:35:30 +0000

    graduation final project dbmigr

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,14 +1,2 @@
 class Photographer < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    mount_uploader :video, VideoUploader
-    has_many :comments, dependent: :destroy
-    belongs_to :user
-    validates :image, :video, presence: true
-    def self.search(search, id)
-      if search
-        where(['name LIKE ?', "%#{search}%"])
-      else
-        where("name LIKE ?", "%#{name}%")
-      end
-    end
 end
